fcc7d82634d8857b02d08bc42c2cef2882657d8a
Anna Beddow <anna.beddow@gmail.com>
2023-09-15 13:02:04 +0100

    Switch headers to query param

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,25 @@ addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event: FetchEvent) {
   // Log service version
-  console.log("FASTLY_SERVICE_VERSION:", env('FASTLY_SERVICE_VERSION') || 'local');
+  console.log(
+    "FASTLY_SERVICE_VERSION:",
+    env("FASTLY_SERVICE_VERSION") || "local"
+  );
 
   const req = event.request;
 
   const gripSig = req.headers.get("Grip-Sig");
   if (gripSig) {
-    const contentType = req.headers.get("Content-Type")
-    const channel = req.headers.get("x-gu-path")
+    // headers dont exist so lets use query params
+    const url = new URL(req.url);
+    const contentType = url.searchParams.get("contentType");
+    const channel = url.searchParams.get("channel");
+    // const contentType = req.headers.get("Content-Type")
+    // const channel = req.headers.get("x-gu-path")
     console.log({ contentType, channel });
 
-    if (!channel) return new Response("x-gu-path header required", { status: 400 })
+    if (!channel)
+      return new Response("x-gu-path header required", { status: 400 });
     if (contentType != "application/websocket-events") {
       // assume this is server sent event
       return new Response("welcome\n", {
@@ -27,33 +35,32 @@ async function handleRequest(event: FetchEvent) {
           "Content-Type": "text/event-stream",
           "Grip-Hold": "stream",
           "Grip-Channel": channel,
-        }
-      })
+        },
+      });
     }
 
-    const body = await req.text()
+    const body = await req.text();
     console.log("body: ", body);
     if (body.startsWith("OPEN")) {
-      const bodyStr = `OPEN\r\nTEXT 27\r\nc:{\"type\":\"subscribe\",\"channel\":\"${channel}\"}`
-      return new Response(
-        toUint8Array(bodyStr),
-        {
-          status: 200,
-          headers: {
-            "Content-Type": "application/websocket-events",
-            "Sec-WebSocket-Extensions": "grip; message-prefix=\"\"",
-          }
-        });
+      const bodyStr = `OPEN\r\nTEXT 27\r\nc:{\"type\":\"subscribe\",\"channel\":\"${channel}\"}`;
+      return new Response(toUint8Array(bodyStr), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/websocket-events",
+          "Sec-WebSocket-Extensions": 'grip; message-prefix=""',
+        },
+      });
     } else {
       return new Response(null, {
-        status: 200, headers: {
-          "Content-Type": "application/websocket-events"
-        }
-      })
+        status: 200,
+        headers: {
+          "Content-Type": "application/websocket-events",
+        },
+      });
     }
   } else {
-    console.log("about to create fanout handoff")
-    return createFanoutHandoff(req, "self")
+    console.log("about to create fanout handoff");
+    return createFanoutHandoff(req, "self");
   }
 }
 
