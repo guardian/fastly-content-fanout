2f5360e44ace2222a7176a0c8e9ebd3ccaed05a3
David Lawes <david.lawes@guardian.co.uk>
2023-11-10 14:31:31 +0000

    format and add notes for clarity

--- cdk/lib/eventbridge-to-fanout.test.ts
+++ cdk/lib/eventbridge-to-fanout.test.ts
@@ -1,12 +1,15 @@
-import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { EventbridgeToFanout } from "./eventbridge-to-fanout";
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { EventbridgeToFanout } from './eventbridge-to-fanout';
 
-describe("The EventbridgeToFanout stack", () => {
-  it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new EventbridgeToFanout(app, "EventbridgeToFanout", { stack: "playground", stage: "TEST" });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
-  });
+describe('The EventbridgeToFanout stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new EventbridgeToFanout(app, 'EventbridgeToFanout', {
+			stack: 'playground',
+			stage: 'TEST',
+		});
+		const template = Template.fromStack(stack);
+		expect(template.toJSON()).toMatchSnapshot();
+	});
 });
