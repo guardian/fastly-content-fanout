69f4436aa46b56f179b1ffab176267c942c2ded2
David Lawes <david.lawes@guardian.co.uk>
2023-11-10 14:31:31 +0000

    format and add notes for clarity

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,15 @@
-import "source-map-support/register";
-import { GuRoot } from "@guardian/cdk/lib/constructs/root";
-import { EventbridgeToFanout } from "../lib/eventbridge-to-fanout";
+import 'source-map-support/register';
+import { GuRoot } from '@guardian/cdk/lib/constructs/root';
+import { EventbridgeToFanout } from '../lib/eventbridge-to-fanout';
 
 const app = new GuRoot();
-new EventbridgeToFanout(app, "EventbridgeToFanout-euwest-1-CODE", { stack: "playground", stage: "CODE", env: { region: "eu-west-1" } });
-new EventbridgeToFanout(app, "EventbridgeToFanout-euwest-1-PROD", { stack: "playground", stage: "PROD", env: { region: "eu-west-1" } });
+new EventbridgeToFanout(app, 'EventbridgeToFanout-euwest-1-CODE', {
+	stack: 'playground',
+	stage: 'CODE',
+	env: { region: 'eu-west-1' },
+});
+new EventbridgeToFanout(app, 'EventbridgeToFanout-euwest-1-PROD', {
+	stack: 'playground',
+	stage: 'PROD',
+	env: { region: 'eu-west-1' },
+});
