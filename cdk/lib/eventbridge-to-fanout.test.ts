259d3662e8d44948038d478cb89e0a8d0d9aedca
David Lawes <david.lawes@guardian.co.uk>
2023-11-17 16:13:21 +0000

    update stack to cms-fronts

--- cdk/lib/eventbridge-to-fanout.test.ts
+++ cdk/lib/eventbridge-to-fanout.test.ts
@@ -2,11 +2,11 @@ import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { EventbridgeToFanout } from './eventbridge-to-fanout';
 
-describe('The EventbridgeToFanout stack', () => {
+describe('The EventBridgeToFanout stack', () => {
 	it('matches the snapshot', () => {
 		const app = new App();
-		const stack = new EventbridgeToFanout(app, 'EventbridgeToFanout', {
-			stack: 'playground',
+		const stack = new EventbridgeToFanout(app, 'EventBridgeToFanout', {
+			stack: 'stack',
 			stage: 'TEST',
 		});
 		const template = Template.fromStack(stack);
