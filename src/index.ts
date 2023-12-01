bf64e484b70496acaa61cb4db4e242ffe0069660
David Lawes <david.lawes@guardian.co.uk>
2023-12-01 16:18:46 +0000

    refactor

--- src/index.ts
+++ src/index.ts
@@ -3,72 +3,57 @@
 import { env } from "fastly:env";
 import { createFanoutHandoff } from "fastly:fanout";
 
-const X_GU_PROTOCOL = "x-gu-protocol";
-
 // Use this fetch event listener to define your main request handling logic.
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
-async function handleRequest(event: FetchEvent) {
+async function handleRequest({ request }: FetchEvent) {
   // Log service version
   console.log(
     "FASTLY_SERVICE_VERSION:",
     env("FASTLY_SERVICE_VERSION") || "local"
   );
 
-  const req = event.request;
+  const channel = new URL(request.url).pathname;
 
-  const gripSig = req.headers.get("Grip-Sig");
-  const url = new URL(req.url);
-  console.log(req.url, url, Array.from(req.headers.entries()))
-  if (gripSig) {
-    // headers dont exist so lets use query params
-    const channel = url.pathname;
-    const contentType = req.headers.get("Accept") //?
+  if (!channel || channel === "/")
+    return new Response("No path provided.", { status: 400 });
 
-    if (!channel || channel === "/")
-      return new Response("No path provided.", { status: 400 });
+  if (!request.headers.has("Grip-Sig")) {
+    console.log("about to create fanout handoff");
+    return createFanoutHandoff(request, "self");
+  }
 
-    if (contentType === "https:") {
-      // assume this is server sent event
-      return new Response("welcome\n", {
+  if (request.headers.get("Accept") === "application/websocket-events") {
+    const body = await request.text();
+    if (body.startsWith("OPEN")) {
+      const subscribePayload = { type: "subscribe", channel };
+      const bodyStr = `OPEN\r\nTEXT 27\r\nc:${JSON.stringify(subscribePayload)}`;
+      return new Response(toUint8Array(bodyStr), {
         status: 200,
         headers: {
-          "Content-Type": "text/event-stream",
-          "Grip-Hold": "stream",
-          "Grip-Channel": channel,
+          "Content-Type": "application/websocket-events",
+          "Sec-WebSocket-Extensions": 'grip; message-prefix=""',
+        },
+      });
+    } else {
+      return new Response(null, {
+        status: 200,
+        headers: {
+          "Content-Type": "application/websocket-events",
         },
       });
     }
-
-    if (contentType === "wss:") {
-      const body = await req.text();
-      if (body.startsWith("OPEN")) {
-        const subscribePayload = { type: "subscribe", channel };
-        const bodyStr = `OPEN\r\nTEXT 27\r\nc:${JSON.stringify(subscribePayload)}`;
-        return new Response(toUint8Array(bodyStr), {
-          status: 200,
-          headers: {
-            "Content-Type": "application/websocket-events",
-            "Sec-WebSocket-Extensions": 'grip; message-prefix=""',
-          },
-        });
-      } else {
-        return new Response(null, {
-          status: 200,
-          headers: {
-            "Content-Type": "application/websocket-events",
-          },
-        });
-      }
-    }
-    return new Response("Invalid protocol (use https for server sent events or wss for websockets)", {
-      status: 400,
-    });
-  } else {
-    console.log("about to create fanout handoff");
-    req.headers.set(X_GU_PROTOCOL, url.protocol)
-    return createFanoutHandoff(req, "self");
   }
+
+  // assume this is server sent event
+  return new Response("welcome\n", { // TODO: can we send null for body?
+    status: 200,
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Grip-Hold": "stream",
+      "Grip-Channel": channel,
+    },
+  });
 }
 
 function toUint8Array(str: string) {
