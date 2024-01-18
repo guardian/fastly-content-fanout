c631d9e8e104a048d3873a5bf4ed31d270961649
David Lawes <david.lawes@guardian.co.uk>
2024-01-18 12:26:56 +0000

    update capi stack value

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,7 +3,7 @@ import { GuRoot } from '@guardian/cdk/lib/constructs/root';
 import { EventbridgeToFanout } from '../lib/eventbridge-to-fanout';
 
 const FRONTS_STACK = 'cms-fronts';
-const CAPI_STACK = 'content-api-crier-v2'; // crier also has a preview stack (content-api-crier-v2-preview) but we don't think this is needed right now
+const CAPI_STACK = 'content-api-fastly-cache-purger'; // crier also has a preview stack (content-api-crier-v2-preview) but we don't think this is needed right now
 const app = new GuRoot();
 
 const env = { region: 'eu-west-1' };
